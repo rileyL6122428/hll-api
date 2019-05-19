a2f1668bc2f05e1d514c67f6f9654d5826890c51
Riley Littlefield <rileylittlefield@ymail.com>
2019-05-19 00:00:48 -0400

    initial commit

---
+++ src/main/java/com/example/hllapi/HllApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hllapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HllApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HllApiApplication.class, args);
+	}
+
+}
