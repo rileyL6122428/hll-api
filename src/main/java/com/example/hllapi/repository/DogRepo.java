d837dfe6779ce21a4dfae49966ed91e16747d7eb
Riley Littlefield <rileylittlefield@ymail.com>
2019-07-01 23:00:08 -0400

    deletes dead code

--- src/main/java/com/example/hllapi/repository/DogRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.hllapi.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.hllapi.model.Dog;
-
-public interface DogRepo extends MongoRepository<Dog, String> {
-
-}
