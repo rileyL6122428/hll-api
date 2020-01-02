6c954e1ad5aded361b81eed0e7c07b44c4c4bd9a
Riley Littlefield <rileylittlefield@ymail.com>
2020-01-01 21:26:45 -0500

    commits working impl of upload on linux

--- src/main/java/com/example/hllapi/track/impl/Mp3agicTrackParser.java
+++ src/main/java/com/example/hllapi/track/impl/Mp3agicTrackParser.java
@@ -5,41 +5,38 @@ import java.nio.file.Files;
 import java.util.Random;
 
 import com.example.hllapi.track.TrackMetadataParser;
+import com.mpatric.mp3agic.Mp3File;
 
-import net.bramp.ffmpeg.FFprobe;
-import net.bramp.ffmpeg.probe.FFmpegFormat;
-import net.bramp.ffmpeg.probe.FFmpegProbeResult;
+public class Mp3agicTrackParser implements TrackMetadataParser {
 
-public class FfmpegTrackParser implements TrackMetadataParser {
-	
 	private String tempFilepath;
-	private FFprobe ffprobe;
 	private Random random;
 	
-	public FfmpegTrackParser(
+	public Mp3agicTrackParser(
 		String tempFilepath,
-		FFprobe ffprobe,
 		Random random
 	) {
 		this.tempFilepath = tempFilepath;
-		this.ffprobe = ffprobe;
 		this.random = random;
 	}
 	
+	@Override
 	public double getDuration(byte[] fileBytes) throws Exception {
 		String tempFilePath = tempFilepath + "/temp." + random.nextFloat() + ".test";
 		File tempFile = new File(tempFilePath);
 		Files.write(tempFile.toPath(), fileBytes);
 		
-		tempFile.setExecutable(true, false);
-		tempFile.setReadable(true, false);
-		tempFile.setWritable(true, false);
+//		tempFile.setExecutable(true, false);
+//		tempFile.setReadable(true, false);
+//		tempFile.setWritable(true, false);
 		
-		FFmpegProbeResult probeResult = ffprobe.probe(tempFilePath);
-		FFmpegFormat format = probeResult.getFormat();
+		Mp3File mp3File = new Mp3File(tempFile.getAbsolutePath());
+		
+		double duration = (double)mp3File.getLengthInMilliseconds() / 1000d;
 		
 		Files.delete(tempFile.toPath());
-		return format.duration;
+		
+		return duration;
 	}
 
 }
